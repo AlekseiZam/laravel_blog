3c54b50698fcf49eb8c82102075f1b25708b80b1
AlekseiZam <aleksei.zam2000@yandex.ru>
2024-03-12 20:02:44 +0400

    "start"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('/blog')->group(function () {
+
+    Route::get('/', function () {
+        return view('terms');
+    })->name('index');
+
+    Route::get('/login', function () {
+        return view('blog.login');
+    })->name('login');
+
+    Route::get('/register', function () {
+        return view('blog.register');
+    })->name('register');
+
+    Route::get('/profile', function () {
+        return view('blog.profile');
+    })->name('profile');
+
 });
